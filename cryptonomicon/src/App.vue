d39906fd4859c81b7f32dcc03c28f66a5e5db317
Valerii Chystiakov <hnu.kn20.2.chistyakov@gmail.com>
2023-09-18 11:30:56 +0300

    fixing a lot of bugs

--- cryptonomicon/src/App.vue
+++ cryptonomicon/src/App.vue
@@ -192,7 +192,7 @@ export default {
 
   data() {
     return {
-      ticker: "default",
+      ticker: "",
       tickers: [],
       sel: null,
       graph: [],
@@ -201,24 +201,39 @@ export default {
   },
 
   methods: {
+    fetchAndProcessTickerData(newTicker) {
+      fetch(
+        `https://min-api.cryptocompare.com/data/price?fsym=${newTicker.name}&tsyms=USD&api_key=89070d6ae5c802d1bbf5007c8e6b1644f9d151ff34df51d382c9d017f30b3a2e`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const tickerToUpdate = this.tickers.find(
+            (number) => number.name === newTicker.name
+          );
+          if (tickerToUpdate) {
+            tickerToUpdate.price =
+              data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+          }
+
+          if (this.sel?.name === newTicker.name) {
+            this.graph.push(data.USD);
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    },
     add() {
       const newTicker = { name: this.ticker, price: "-" };
       if (this.listOfNames.indexOf(newTicker.name) === -1) {
         this.listOfNames.push(newTicker.name);
         this.tickers.push(newTicker);
-        setInterval(async () => {
-          const f = await fetch(
-            `https://min-api.cryptocompare.com/data/price?fsym=${newTicker.name}&tsyms=USD&api_key=89070d6ae5c802d1bbf5007c8e6b1644f9d151ff34df51d382c9d017f30b3a2e`
-          );
-          const data = await f.json();
-          this.tickers.find((number) => number.name === newTicker.name).price =
-            data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-          if (this.sel?.name === newTicker.name) {
-            this.graph.push(data.USD);
-          }
+        /* eslint-disable-next-line no-unused-vars */
+        let intervalID = setInterval(() => {
+          this.fetchAndProcessTickerData(newTicker);
         }, 3000);
-        this.ticker = "";
       }
+      this.ticker = "";
     },
 
     select(ticker) {
@@ -228,6 +243,12 @@ export default {
 
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter((number) => number != tickerToRemove);
+      setTimeout(function () {
+        clearInterval(this.intervalID);
+      }, 0);
+      this.listOfNames = this.listOfNames.filter(
+        (name) => name != tickerToRemove.name
+      );
     },
 
     normalizeGraph() {
